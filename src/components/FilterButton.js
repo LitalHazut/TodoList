a091b941df06e2b1603d363f4e2651d2b127e4e0
lital hazot <lital7h@gmail.com>
2022-06-25 20:11:23 +0300

    add filter button

---
+++ src/components/FilterButton.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+function FilterButton(props) {
+    const [filter, setFilter] = useState('All');
+    
+    const FILTER_MAP = {
+        All: () => true,
+        Active: task => !task.completed,
+        Completed: task => task.completed
+    };
+    const FILTER_NAMES = Object.keys(FILTER_MAP);
+    
+    return (
+      <button
+        type="button"
+        className="btn toggle-btn"
+        aria-pressed={props.isPressed}
+        onClick={() => props.setFilter(props.name)}
+      >
+        <span className="visually-hidden">Show </span>
+        <span>{props.name}</span>
+        <span className="visually-hidden"> tasks</span>
+      </button>
+    );
+
+
+
+
+
+
+    
+  }
+  export default FilterButton;
